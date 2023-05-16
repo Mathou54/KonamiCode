9ee5045a5a48538bcd98571926fa7294ac5ad6be
Mathou54 <matt54425+github.com@gmail.com>
2023-05-16 20:31:20 +0200

    feat: update to latest angular (from new project)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,10 +18,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'KonamiCode2'`, () => {
+  it(`should have as title 'KonamiCode'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('KonamiCode2');
+    expect(app.title).toEqual('KonamiCode');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'KonamiCode2';
+  title = 'KonamiCode';
 }
